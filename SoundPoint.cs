a481122cbcc60d0b15b84651c34d9c8e32e72b9d
RomanHome <kr.rm@ya.ru>
2019-12-08 23:59:39 +0300

    Уменьшено трескание

--- SoundPoint.cs
+++ SoundPoint.cs
@@ -19,8 +19,13 @@ namespace SoundMap
 
 		private double FVolume = 0;
 		private double FFrequency = 0;
-		private double FPhaseOffset = 0;
+
+		private double FNewFrequency = double.NaN;
 		private double FOldFrequency = double.NaN;
+		private double FTimeOffset = 0; 
+
+		private double FTransitionTimeStart = double.NaN;
+		private double FTransitionTimeLength = double.NaN;
 
 		public event Action<SoundPoint> RemoveSelf;
 
@@ -115,6 +120,7 @@ namespace SoundMap
 				if (FFrequency != value)
 				{
 					FOldFrequency = FFrequency;
+					FNewFrequency = value;
 					FFrequency = value;
 					NotifyPropertyChanged(nameof(Frequency));
 				}
@@ -128,49 +134,75 @@ namespace SoundMap
 
 		public double GetValue(double ATime)
 		{
-			switch (Kind)
+			var oldValue = Math.Sin(FOldFrequency * (ATime - FTimeOffset) * Math.PI * 2);
+
+			if (!double.IsNaN(FNewFrequency))
 			{
-				case PointKind.Static:
-					if (!double.IsNaN(FOldFrequency) && (FOldFrequency != 0))
-					{
-						// Получаю значение каким бы оно должно быть
-						var oldV = Math.Sin(FOldFrequency * ATime * Math.PI * 2 + FPhaseOffset);
-						// Вычисление угла чтобы текущее значение...
-						FPhaseOffset = Math.Asin(oldV) - 2 * Math.PI * Frequency * ATime;
-						FOldFrequency = double.NaN;
-					}
-					return Math.Sin(Frequency * ATime * Math.PI * 2 + FPhaseOffset);
-				case PointKind.Bell:
-					if (double.IsNaN(FStartTime))
-						FStartTime = ATime;
+				if (double.IsNaN(FTransitionTimeStart))
+				{
+					FTransitionTimeLength = 4 / FNewFrequency;
+					FTransitionTimeStart = ATime;
+				}
 
-					var d = ATime - FStartTime;
-					d = 1 / (1 + d*d);
+				var newValue = Math.Sin(FNewFrequency * (ATime - FTransitionTimeStart) * Math.PI * 2);
 
-					if (d < 0.1)
-						DoRemoveSelf();
+				var transPct = (ATime - FTransitionTimeStart) / FTransitionTimeLength;
 
-					return  d*Math.Sin(Frequency * ATime * Math.PI * 2);
-				case PointKind.Saw:
-					// Частота - это количество колебаний в секунду. Колебание необходимо разделить на 3 фазы.
-					// А для этого нужен вещественный остаток времени фазы
-					var z = Math.Truncate(ATime * Frequency);
-					var rem = ATime - z/ Frequency;
-					// 1/Frequency время одного колебания 1/F/4 - время одной фазы
-					var f = 1 / Frequency;
-					var f4 = f / 4;
-					if (rem < f4)
-						return rem/f4;
-					if (rem < f4 * 3)
-					{
-						rem -= f4;
-						return 1 - rem / f4;
-					}
-					rem -= f4 * 3;
-					return -1 + rem / f4;
-				default:
-					throw new NotImplementedException();
+				oldValue = (1 - transPct) * oldValue + transPct * newValue;
+				if (ATime - FTransitionTimeStart >= FTransitionTimeLength)
+				{
+					FTimeOffset = FTransitionTimeStart;
+					FOldFrequency = FNewFrequency;
+					FNewFrequency = double.NaN;
+					FTransitionTimeStart = double.NaN;
+				}
 			}
+
+			return oldValue;
+
+			//switch (Kind)
+			//{
+			//	case PointKind.Static:
+			//		if (!double.IsNaN(FOldFrequency) && (FOldFrequency != 0))
+			//		{
+			//			// Получаю значение каким бы оно должно быть
+			//			var oldV = Math.Sin(FOldFrequency * ATime * Math.PI * 2 + FPhaseOffset);
+			//			// Вычисление угла чтобы текущее значение...
+			//			FPhaseOffset = Math.Asin(oldV) - 2 * Math.PI * Frequency * ATime;
+			//			FOldFrequency = double.NaN;
+			//		}
+			//		return Math.Sin(Frequency * ATime * Math.PI * 2 + FPhaseOffset);
+			//	case PointKind.Bell:
+			//		if (double.IsNaN(FStartTime))
+			//			FStartTime = ATime;
+
+			//		var d = ATime - FStartTime;
+			//		d = 1 / (1 + d*d);
+
+			//		if (d < 0.1)
+			//			DoRemoveSelf();
+
+			//		return  d*Math.Sin(Frequency * ATime * Math.PI * 2);
+			//	case PointKind.Saw:
+			//		// Частота - это количество колебаний в секунду. Колебание необходимо разделить на 3 фазы.
+			//		// А для этого нужен вещественный остаток времени фазы
+			//		var z = Math.Truncate(ATime * Frequency);
+			//		var rem = ATime - z/ Frequency;
+			//		// 1/Frequency время одного колебания 1/F/4 - время одной фазы
+			//		var f = 1 / Frequency;
+			//		var f4 = f / 4;
+			//		if (rem < f4)
+			//			return rem/f4;
+			//		if (rem < f4 * 3)
+			//		{
+			//			rem -= f4;
+			//			return 1 - rem / f4;
+			//		}
+			//		rem -= f4 * 3;
+			//		return -1 + rem / f4;
+			//	default:
+			//		throw new NotImplementedException();
+			//}
 		}
 
 		private void DoRemoveSelf()
